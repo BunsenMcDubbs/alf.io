40daea0ba52d9adc169417b01ff66ee117e26940
Andrew Dai <theman@andrewdai.co>
2016-07-12 20:54:35 -0400

    implement mailer-like switching logic for locationmanagers #127

--- src/main/java/alfio/manager/location/DefaultLocationManager.java
+++ src/main/java/alfio/manager/location/DefaultLocationManager.java
@@ -20,62 +20,41 @@ import alfio.config.Initializer;
 import alfio.manager.system.ConfigurationManager;
 import alfio.model.system.Configuration;
 import alfio.model.system.ConfigurationKeys;
-import com.google.maps.GeoApiContext;
-import com.google.maps.GeocodingApi;
-import com.google.maps.TimeZoneApi;
-import com.google.maps.model.LatLng;
+import lombok.AllArgsConstructor;
 import org.apache.commons.lang3.tuple.Pair;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.cache.annotation.Cacheable;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 
-import java.util.Optional;
 import java.util.TimeZone;
-import java.util.concurrent.atomic.AtomicReference;
 
 @Component
+@AllArgsConstructor
 @Profile(Initializer.PROFILE_LIVE)
 public class DefaultLocationManager implements LocationManager {
 
-    private static final AtomicReference<GeoApiContext> CTX = new AtomicReference<>();
     private final ConfigurationManager configurationManager;
+    private final GoogleMapsLocationManager googleMapsLocationManager;
+    private final MockLocationManager mockLocationManager;
 
-    @Autowired
-    public DefaultLocationManager(ConfigurationManager configurationManager) {
-         this.configurationManager = configurationManager;
+    // TODO change this to be more robust and flexible (like the Mailer system) when there are more managers
+    private LocationManager getLocationManager() {
+        return configurationManager.getStringConfigValue(
+            Configuration.getSystemConfiguration(ConfigurationKeys.MAPS_SERVER_API_KEY))
+            .isPresent() ? googleMapsLocationManager : mockLocationManager;
     }
 
     @Override
-    @Cacheable
     public Pair<String, String> geocode(String address) {
-        return Optional.ofNullable(GeocodingApi.geocode(getApiContext(), address).awaitIgnoreError())
-                    .filter(r -> r.length > 0)
-                    .map(r -> r[0].geometry.location)
-                    .map(l -> Pair.of(Double.toString(l.lat), Double.toString(l.lng)))
-                    .orElseThrow(() -> new LocationNotFound("No location found for address " + address));
-    }
-
-    private GeoApiContext getApiContext() {
-        GeoApiContext ctx = CTX.get();
-        if(ctx == null) {
-            ctx = new GeoApiContext().setApiKey(configurationManager.getRequiredValue(Configuration.getSystemConfiguration(ConfigurationKeys.MAPS_SERVER_API_KEY)));
-            CTX.compareAndSet(null, ctx);
-        }
-        return CTX.get();
+        return getLocationManager().geocode(address);
     }
 
     @Override
-    @Cacheable
     public TimeZone getTimezone(Pair<String, String> location) {
-        return getTimezone(location.getLeft(), location.getRight());
+        return getLocationManager().getTimezone(location);
     }
 
     @Override
-    @Cacheable
     public TimeZone getTimezone(String latitude, String longitude) {
-        return Optional.ofNullable(TimeZoneApi.getTimeZone(getApiContext(), new LatLng(Double.valueOf(latitude), Double.valueOf(longitude))).awaitIgnoreError())
-                .orElseThrow(() -> new LocationNotFound(String.format("No TimeZone found for location having coordinates: %s,%s", latitude, longitude)));
-
+        return getLocationManager().getTimezone(latitude, longitude);
     }
 }
